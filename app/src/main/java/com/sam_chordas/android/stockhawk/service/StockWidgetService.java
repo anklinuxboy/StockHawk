6b2466f1ec18d17bf945beb7b77218aab7fbbb02
Ankit Sharma <ankitsharma.nov11@gmail.com>
2017-02-07 22:06:36 -0500

    Followed Android Dev Guide

--- app/src/main/java/com/sam_chordas/android/stockhawk/service/StockWidgetService.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/service/StockWidgetService.java
@@ -4,6 +4,8 @@ import android.appwidget.AppWidgetManager;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.os.Bundle;
+import android.util.Log;
 import android.widget.RemoteViews;
 import android.widget.RemoteViewsService;
 
@@ -11,6 +13,7 @@ import com.sam_chordas.android.stockhawk.R;
 import com.sam_chordas.android.stockhawk.data.QuoteColumns;
 import com.sam_chordas.android.stockhawk.data.QuoteProvider;
 import com.sam_chordas.android.stockhawk.data.StockWidgetItem;
+import com.sam_chordas.android.stockhawk.widget.StockHawkAppWidgetProvider;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,6 +25,7 @@ import java.util.List;
 public class StockWidgetService extends RemoteViewsService {
     @Override
     public RemoteViewsFactory onGetViewFactory(Intent intent) {
+        Log.d("TAG", "in remote views factory");
         return new ListRemoteViewsFactory(this.getApplicationContext(), intent);
     }
 }
@@ -44,6 +48,7 @@ class ListRemoteViewsFactory implements RemoteViewsService.RemoteViewsFactory {
 
     @Override
     public void onCreate() {
+        System.out.println("Hello");
         Cursor cursor = context.getContentResolver().query(QuoteProvider.Quotes.CONTENT_URI,
                 null,
                 null,
@@ -74,6 +79,11 @@ class ListRemoteViewsFactory implements RemoteViewsService.RemoteViewsFactory {
         RemoteViews rv = new RemoteViews(context.getPackageName(), R.layout.widget_list_item);
         rv.setTextViewText(R.id.stock_symbol, widgetItemList.get(position).getStockSymbol());
         rv.setTextViewText(R.id.stock_price, widgetItemList.get(position).getStockPrice());
+
+        Bundle extras = new Bundle();
+        extras.putInt(StockHawkAppWidgetProvider.EXTRA_ITEM, position);
+        Intent fillInIntent = new Intent();
+        fillInIntent.putExtras(extras);
 
         return rv;
     }
--- app/src/main/java/com/sam_chordas/android/stockhawk/widget/StockHawkAppWidgetProvider.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/widget/StockHawkAppWidgetProvider.java
@@ -1,11 +1,14 @@
 package com.sam_chordas.android.stockhawk.widget;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.util.Log;
 import android.widget.RemoteViews;
+import android.widget.Toast;
 
 import com.sam_chordas.android.stockhawk.R;
 import com.sam_chordas.android.stockhawk.service.StockWidgetService;
@@ -15,6 +18,35 @@ import com.sam_chordas.android.stockhawk.service.StockWidgetService;
  */
 
 public class StockHawkAppWidgetProvider extends AppWidgetProvider {
+
+    public static final String TOAST_ACTION = "com.sam_chordas.android.stockhawk.TOAST_ACTION";
+    public static final String EXTRA_ITEM = "com.sam_chordas.android.stockhawk.EXTRA_ITEM";
+
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        super.onDeleted(context, appWidgetIds);
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        super.onDisabled(context);
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        super.onEnabled(context);
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        AppWidgetManager mgr = AppWidgetManager.getInstance(context);
+        if (intent.getAction().equals(TOAST_ACTION)) {
+            int appWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,
+                    AppWidgetManager.INVALID_APPWIDGET_ID);
+            int viewIndex = intent.getIntExtra(EXTRA_ITEM, 0);
+        }
+        super.onReceive(context, intent);
+    }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
@@ -26,6 +58,15 @@ public class StockHawkAppWidgetProvider extends AppWidgetProvider {
             RemoteViews remoteViews = new RemoteViews(context.getPackageName(),
                     R.layout.stockhawk_appwidget_layout);
             remoteViews.setRemoteAdapter(appWidgetIds[i], R.id.widget_list, intent);
+            Log.d("TAG", "after set remote adapter");
+
+            Intent toastIntent = new Intent(context, StockHawkAppWidgetProvider.class);
+            toastIntent.setAction(StockHawkAppWidgetProvider.TOAST_ACTION);
+            toastIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetIds[i]);
+            intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
+            PendingIntent toastPendingIntent = PendingIntent.getBroadcast(context, 0, toastIntent,
+                    PendingIntent.FLAG_UPDATE_CURRENT);
+            remoteViews.setPendingIntentTemplate(R.id.widget_list, toastPendingIntent);
 
             appWidgetManager.updateAppWidget(appWidgetIds[i], remoteViews);
         }
